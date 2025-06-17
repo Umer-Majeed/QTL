5fe2a81c96ea15b42ce1979788dd5301fbd7ddf6
Umer Majeed <umarmajeed786k@gmail.com>
2025-06-17 23:21:30 +0500

    for loop adding  main.cpp

--- main.cpp
+++ main.cpp
@@ -4,3 +4,13 @@ using namespace std;
 int main (){
 char repeat;
 }
+
+do{
+  int balance = 5000;
+  int withdraw;
+  
+          // 1. FOR LOOP – Last 5 Transactions // yeh foor loop wala section hai jis mein hum log is ko loop karin gy 5 tak taky 5000 tak ki transactions history print ho 
+  cout<<"\nLast 5 Transaction: \n";
+  for(int i = 5; i<=5 ;i++){
+  cout << "transaction" <<i<< ":Rs."<<i*100<<endl;
+  }// there the for loop is done!
