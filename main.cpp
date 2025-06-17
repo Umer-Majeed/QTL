45059f2a393d14f08c648f1440493639d4954396
Umer Majeed <umarmajeed786k@gmail.com>
2025-06-17 23:32:45 +0500

    adding the withdraw feature with the while loop main.cpp

--- main.cpp
+++ main.cpp
@@ -14,3 +14,24 @@ do{
   for(int i = 5; i<=5 ;i++){
   cout << "transaction" <<i<< ":Rs."<<i*100<<endl;
   }// there the for loop is done!
+
+// now we need to add while loop where we need deal with withdarw the money! 
+
+cout<<"\nBalance:Rs."<<balance<<endl;
+cout<<"Enter the amount of withdraw (0 to stop):";
+cin>>withdraw;
+  
+  while(withdraw!=0 && balance >= withdraw){
+  
+balance-=withdraw;
+cout <<"Withdraw: Rs.  "<<withdraw<<",New Balance : Rs."<<balance<<endl;  
+
+if(balance == 0){
+cout << " balance is empty!\n";
+break;
+}
+
+cout<< " Enter the amount of withdraw (stop press 0):";
+cin>>withdraw 
+  
+}
